acb22c82c46fe720686ba63cce3b075f9ba8dddd
Daniela Brenes <danibreo6@gmail.com>
2021-09-04 22:31:10 -0600

    Largo de identificadores listo

--- Interprete/src/main/java/com/robotichand/Interprete/ast/VarAssign.java
+++ Interprete/src/main/java/com/robotichand/Interprete/ast/VarAssign.java
@@ -19,30 +19,39 @@ public class VarAssign implements ASTNode {
 		
 boolean var_exists = symbolTable.containsKey(name);
 		
-		if (expression.execute(symbolTable) == null){
-			System.out.println("The assigned variable is not defined");
-		}
-		else if (!var_exists) {
-			symbolTable.put(name, expression.execute(symbolTable));
-		}
-		else {
-			 Object original_class = (symbolTable.get(name)).getClass();
-			
-			if ((expression.execute(symbolTable)).getClass() == original_class) {
+		int length = name.length();
+		
+		if (length >= 3 && length <= 15) {
+			if (expression.execute(symbolTable) == null){
+				System.out.println("The assigned variable is not defined");
+			}
+			else if (!var_exists) {
 				symbolTable.put(name, expression.execute(symbolTable));
 			}
 			else {
-				String type;
-				if (original_class == Boolean.class) {
-					type = "boolean";
+				 Object original_class = (symbolTable.get(name)).getClass();
+				
+				if ((expression.execute(symbolTable)).getClass() == original_class) {
+					symbolTable.put(name, expression.execute(symbolTable));
 				}
 				else {
-					type = "integer";
+					String type;
+					if (original_class == Boolean.class) {
+						type = "boolean";
+					}
+					else {
+						type = "integer";
+					}
+					System.out.println("Expected " + type + " type. Can't convert different types." );
 				}
-				System.out.println("Expected " + type + " type. Can't convert different types." );
 			}
 		}
-		
+		else {
+			System.out.println("Variables identifiers should be of length between 3 and 15 characters.");
+		}
+
+
+				
 		return null;
 	}
 
