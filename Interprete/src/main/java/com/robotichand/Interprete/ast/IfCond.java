ec74613636b5d914deb32748c442943e9819d138
BadStone3100 <48070861+Oscarmendez825@users.noreply.github.com>
2021-09-05 20:47:51 -0600

    optimización x2 del move e if

--- Interprete/src/main/java/com/robotichand/Interprete/ast/IfCond.java
+++ Interprete/src/main/java/com/robotichand/Interprete/ast/IfCond.java
@@ -22,42 +22,47 @@ public class IfCond implements ASTNode {
 	@Override
 	public Object execute(Map<String, Object> symbolTable) {
 		// TODO Auto-generated method stub
-		if ((condition.execute(symbolTable)).getClass() != Boolean.class) {
+		try {
+			if ((condition.execute(symbolTable)).getClass() != Boolean.class) {
 			System.out.println("The if condition must be a boolean");
 			return null;
-		}
-		if ((boolean)condition.execute(symbolTable)) {
-			for (ASTNode n : body) {
-				n.execute(symbolTable);
 			}
-		}
-		else {
-			int cont = 0;
-			boolean cumple = false;
-			for (ASTNode c : elseIfConds) {
-				if ((c.execute(symbolTable)).getClass() != Boolean.class) {
-					System.out.println("The else if condition must be a boolean");
-					return null;
-				}
-				if ((boolean)c.execute(symbolTable)) {
-					cumple = true;
-					break;
-				}
-				cont++;
-			}
-			if (cumple) {
-				List<ASTNode> elseIfBodyCumple = elseIfBodies.get(cont);
-				for (ASTNode n : elseIfBodyCumple) {
+			if ((boolean)condition.execute(symbolTable)) {
+				for (ASTNode n : body) {
 					n.execute(symbolTable);
 				}
 			}
 			else {
-				for(ASTNode n : elseBody) {
-					n.execute(symbolTable);
+				int cont = 0;
+				boolean cumple = false;
+				for (ASTNode c : elseIfConds) {
+					if ((c.execute(symbolTable)).getClass() != Boolean.class) {
+						System.out.println("The else if condition must be a boolean");
+						return null;
+					}
+					if ((boolean)c.execute(symbolTable)) {
+						cumple = true;
+						break;
+					}
+					cont++;
 				}
+				if (cumple) {
+					List<ASTNode> elseIfBodyCumple = elseIfBodies.get(cont);
+					for (ASTNode n : elseIfBodyCumple) {
+						n.execute(symbolTable);
+					}
+				}
+				else {
+					for(ASTNode n : elseBody) {
+						n.execute(symbolTable);
+					}
+				}
+				
+				}
+			}catch(NullPointerException e) {
+				System.out.println("The if or else if condition must be a boolean");
 			}
-			
-		}
+		
 		return null;
 	}
 
--- Interprete/src/main/java/com/robotichand/Interprete/ast/Move.java
+++ Interprete/src/main/java/com/robotichand/Interprete/ast/Move.java
@@ -9,46 +9,54 @@ public class Move implements ASTNode {
 	
 	public Move(String finger, ASTNode condition) {
 		super();
-		this.finger = finger;
-		this.condition = condition;
+		try {
+			this.finger = finger;
+			this.condition = condition;
+		}catch(NullPointerException e) {
+			System.out.println("The parameters must be String and Boolean");
+		}
 	}
 
 
 	@Override
 	public Object execute(Map<String, Object> symbolTable) {
 		// TODO Auto-generated method stub
-		finger = finger.replace("\"", "");
-		if ((condition.execute(symbolTable)).getClass() != Boolean.class) {
-			System.out.println("The orientation parameter must be a boolean");
-			return null;
-		}
-		
-		if (finger.length() == 1 && finger.equals(finger.toUpperCase())) {
-			switch(finger) {
-				case "P":
-					System.out.println("P");
-					break;
-				case "I":
-					System.out.println("I");
-					break;
-				case "M":
-					System.out.println("M");
-					break;
-				case "A":
-					System.out.println("A");
-					break;
-				case "Q":
-					System.out.println("Q");
-					break;
-				case "T":
-					System.out.println("T");
-					break;
-				default:
-					System.out.println("The finger parameter must be: P, I, M, A, Q, T");
+		try {
+			finger = finger.replace("\"", "");
+			if ((condition.execute(symbolTable)).getClass() != Boolean.class) {
+				System.out.println("The orientation parameter must be a boolean");
+				return null;
 			}
-		}
-		else {
-			System.out.println("First parameter must be an uppercase letter.");
+			
+			if (finger.length() == 1 && finger.equals(finger.toUpperCase())) {
+				switch(finger) {
+					case "P":
+						System.out.println("P");
+						break;
+					case "I":
+						System.out.println("I");
+						break;
+					case "M":
+						System.out.println("M");
+						break;
+					case "A":
+						System.out.println("A");
+						break;
+					case "Q":
+						System.out.println("Q");
+						break;
+					case "T":
+						System.out.println("T");
+						break;
+					default:
+						System.out.println("The finger parameter must be: P, I, M, A, Q, T");
+				}
+			}
+			else {
+				System.out.println("First parameter must be an uppercase letter.");
+			}
+		}catch(NullPointerException e) {
+			System.out.println("The parameters must be String and Boolean");
 		}
 		return null;
 	}
